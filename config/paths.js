09255a3ca5164162076b64458e91091c70e43b02
jiro <j.code.public@gmail.com>
2019-07-14 15:37:37 +0900

    update

--- config/paths.js
+++ config/paths.js
@@ -2,7 +2,6 @@
 
 const path = require('path');
 const fs = require('fs');
-const url = require('url');
 
 // Make sure any symlinks in the project folder are resolved:
 // https://github.com/facebook/create-react-app/issues/637
@@ -24,19 +23,6 @@ function ensureSlash(inputPath, needsSlash) {
 
 const getPublicUrl = appPackageJson =>
   envPublicUrl || require(appPackageJson).homepage;
-
-// We use `PUBLIC_URL` environment variable or "homepage" field to infer
-// "public path" at which the app is served.
-// Webpack needs to know it to put the right <script> hrefs into HTML even in
-// single-page apps that may serve index.html for nested URLs like /todos/42.
-// We can't use a relative path in HTML because we don't want to load something
-// like /todos/42/static/js/bundle.7289d.js. We have to know the root.
-function getServedPath(appPackageJson) {
-  const publicUrl = getPublicUrl(appPackageJson);
-  const servedUrl =
-    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/');
-  return ensureSlash(servedUrl, true);
-}
 
 const moduleFileExtensions = [
   'web.mjs',
@@ -82,7 +68,6 @@ module.exports = {
   proxySetup: resolveApp('src/setupProxy.js'),
   appNodeModules: resolveApp('node_modules'),
   publicUrl: getPublicUrl(resolveApp('package.json')),
-  servedPath: getServedPath(resolveApp('package.json')),
 };
 
 
--- src/main/index.ts
+++ src/main/index.ts
@@ -2,7 +2,16 @@ import { app, ipcMain, BrowserWindow } from 'electron'
 
 const onReady = async () => {
   let win: BrowserWindow | null
+  win = new BrowserWindow({width: 400, height: 300});
+  win.loadURL('http://loclahost:8080');
+
+  // ChromiumのDevツールを開く
+  win.webContents.openDevTools();
+
+  win.on('closed', function() {
+    win = null;
+  });
 
 }
 app.once('ready', onReady)
-app.on('window-all-closed', () => { app.quite() })
+app.on('window-all-closed', () => { app.quit() })
